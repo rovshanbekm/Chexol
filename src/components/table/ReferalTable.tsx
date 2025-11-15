937aed0fe86f62f056e74e71896acc59c96e98be
rovshanbekm <rmirvoxitov@gmail.com>
2025-11-15 18:06:15 +0500

    Update ReferalTable.tsx

--- src/components/table/ReferalTable.tsx
+++ src/components/table/ReferalTable.tsx
@@ -10,7 +10,6 @@ import { useGetUsersReferall } from '../../hooks'
 
 export const ReferalTable = () => {
     const { data: referrals } = useGetUsersReferall()
-    const referralsArray = Array.isArray(referrals) ? referrals : [];
     return (
         <div className='w-full pt-4'>
             <div className='grid [&>div]:max-h-70 [&>div]:rounded-sm [&>div]:border'>
@@ -21,14 +20,14 @@ export const ReferalTable = () => {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {referralsArray.length === 0 ? (
+                        {referrals.length === 0 ? (
                             <TableRow>
                                 <TableCell className='text-center text-placeholderColor'>
                                     Siz hali hech kimni taklif qilmagansiz
                                 </TableCell>
                             </TableRow>
                         ) : (
-                            referralsArray.map((referall: any) => (
+                            referrals.map((referall: any) => (
                                 <TableRow key={referall.id}>
                                     <TableCell>{referall.full_name}</TableCell>
                                 </TableRow>
