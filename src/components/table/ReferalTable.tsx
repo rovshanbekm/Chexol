540e639305381fe4efe1835a114fecbb205e3a26
rovshanbekm <rmirvoxitov@gmail.com>
2025-11-15 16:50:02 +0500

    fix bag in referralTable

--- src/components/table/ReferalTable.tsx
+++ src/components/table/ReferalTable.tsx
@@ -9,7 +9,8 @@ import {
 import { useGetUsersReferall } from '../../hooks'
 
 export const ReferalTable = () => {
-    const {data:referrals} = useGetUsersReferall()
+    const { data: referrals } = useGetUsersReferall()
+    const referralsArray = Array.isArray(referrals) ? referrals : [];
     return (
         <div className='w-full pt-4'>
             <div className='grid [&>div]:max-h-70 [&>div]:rounded-sm [&>div]:border'>
@@ -20,17 +21,19 @@ export const ReferalTable = () => {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {referrals?.length === 0 ? (
+                        {referralsArray.length === 0 ? (
                             <TableRow>
                                 <TableCell className='text-center text-placeholderColor'>
                                     Siz hali hech kimni taklif qilmagansiz
                                 </TableCell>
                             </TableRow>
-                        ) : referrals?.map((referall:any) => (
-                            <TableRow key={referall.id}>
-                                <TableCell>{referall.paymentMethod}</TableCell>
-                            </TableRow>
-                        ))}
+                        ) : (
+                            referralsArray.map((referall: any) => (
+                                <TableRow key={referall.id}>
+                                    <TableCell>{referall.paymentMethod}</TableCell>
+                                </TableRow>
+                            ))
+                        )}
                     </TableBody>
                 </Table>
             </div>
